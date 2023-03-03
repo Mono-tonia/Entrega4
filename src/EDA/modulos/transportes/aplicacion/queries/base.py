d969c30d8222d69c7cf4a21a25621b10432db4a8
Maria-Gon <69689894+Maria-Gon@users.noreply.github.com>
2023-03-02 22:22:59 -0500

    query Transporte

--- src/EDA/modulos/transportes/aplicacion/queries/base.py
+++ src/EDA/modulos/transportes/aplicacion/queries/base.py
@@ -1,16 +1,16 @@
-from aeroalpes.seedwork.aplicacion.queries import QueryHandler
-from aeroalpes.modulos.vuelos.infraestructura.fabricas import FabricaVista
-from aeroalpes.modulos.vuelos.dominio.fabricas import FabricaVuelos
+from EDA.seedwork.aplicacion.queries import QueryHandler
+from EDA.modulos.transportes.infraestructura.fabricas import FabricaVista
+from EDA.modulos.transportes.dominio.fabricas import FabricaTransporte
 
-class ReservaQueryBaseHandler(QueryHandler):
+class DistribucionQueryBaseHandler(QueryHandler):
     def __init__(self):
         self._fabrica_vista: FabricaVista = FabricaVista()
-        self._fabrica_vuelos: FabricaVuelos = FabricaVuelos()
+        self._fabrica_transporte: FabricaTransporte = FabricaTransporte()
 
     @property
     def fabrica_vista(self):
         return self._fabrica_vista
     
     @property
-    def fabrica_vuelos(self):
-        return self._fabrica_vuelos    
+    def fabrica_transporte(self):
+        return self._fabrica_transporte 
