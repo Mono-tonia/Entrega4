425bf1504044e584503d15de065abb141ae1d19c
LuisaFuentesL <l.fuentesl@uniandes.edu.co>
2023-03-07 22:53:47 -0500

    bye bye bye

--- src/aeroalpes/modulos/vuelos/aplicacion/__init__.py
+++
@@ -1,8 +0,0 @@
-from pydispatch import dispatcher
-
-from .handlers import HandlerOrdenIntegracion
-
-from entregasDeLosAlpes.modulos.productos.dominio.eventos import OrdenRecibida, OrdenProcesada
-
-dispatcher.connect(HandlerOrdenIntegracion.handle_orden_recibida, signal=f'{OrdenRecibida.__name__}Integracion')
-dispatcher.connect(HandlerOrdenIntegracion.handle_orden_procesada, signal=f'{OrdenProcesada.__name__}Integracion')
